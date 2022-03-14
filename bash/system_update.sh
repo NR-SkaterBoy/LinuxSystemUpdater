2bfb1310b475f19c7b39d241c04e5412cce84f60
NR-SkaterBoy <nr.rick.dev@gmail.com>
2022-03-14 13:25:37 +0100

    Published alpha 0.1

--- bash/system_update.sh
+++ bash/system_update.sh
@@ -6,7 +6,6 @@
 
 # Help menu
 if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
-echo -e "\nOptions:\n  --help\t\t\tprint this help message\n  -h\t\t\t\tsame as --help"
 echo " "
 echo "Supported OS:"
 echo -e "\t1.\tUbuntu\t\t\t2.\tKali Linux"
@@ -14,56 +13,54 @@ echo -e "\t3.\tDebian\t\t\t4.\tFedora"
 echo -e "\t5.\tRaspberry\t\t6.\tZorin OS"
 echo -e "\t7.\tElementary OS\t\t8.\tLinux Mint"
 echo -e "\t9.\tArch Linux\t\t10.\tManjaro"
-echo -e "\t11.\tGentoo"
+echo -e "\t11.\tGentoo\t\t\t12\tPop!_os"
+echo -e "\t13.\tSparkyLinux"
 exit
 fi
 
+shell=True
+export SUDO_ASKPASS="myaskpass.sh"
 # Linux Systems
 systems=("ubuntu" "kali" "debian" "fedora" "raspbian" "zorin" "elementary" "linux mint" "arch" "manjaro" "gentoo")
 
 # Linux version ID
 . /etc/os-release
 
-# Script update
-if zenity --question --title="Confirm update" --text="Keep the script up to date! Don't forget to upgrade!\n\nWould you like to update the script?" --no-wrap; then
-    git pull
-fi
-
 
 if [[ $ID == ${systems[0]} ]] || [[ $ID == ${systems[1]} ]] || [[ $ID == ${systems[2]} ]] || [[ $ID == ${systems[3]} ]] || [[ $ID == ${systems[4]} ]] || [[ $ID == ${systems[5]} ]] || [[ $ID == ${systems[6]} ]] || [[ $ID == ${systems[7]} ]] || [[ $ID == ${systems[8]} ]] || [[ $ID == ${systems[9]} ]]; then
     case "$ID" in
     ${systems[0]}) # Ubuntu
-        sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade -y && sudo -A apt autoremove -y
         ;;
     ${systems[1]}) # Kali
-        sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade -y && sudo -A apt full-upgrade -y && sudo -A apt autoremove -y
         ;;
     ${systems[2]}) # Debian
-        sudo apt update && sudo apt upgrade && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade && sudo -A apt autoremove -y
         ;;
     ${systems[3]}) # Fedora
-        sudo dnf upgrade -y
+        sudo -A dnf upgrade -y
         ;;
     ${systems[4]}) # Raspberry
-        sudo apt update && sudo apt upgrade && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade && sudo -A apt autoremove -y
         ;;
     ${systems[5]}) # Zorin
-        sudo apt update && sudo apt upgrade && sudo apt autoremove
+        sudo -A apt update && sudo -A apt upgrade && sudo -A apt autoremove
         ;;
     ${systems[6]}) # Elementary OS
-        sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade -y && sudo -A apt autoremove -y
         ;;
     ${systems[7]}) # Linux Mint
-        sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+        sudo -A apt update && sudo -A apt upgrade -y && sudo -A apt autoremove -y
         ;;
     ${systems[8]}) # Arch
-        sudo pacman -Syyu
+        sudo -A pacman -Syyu
         ;;
     ${systems[9]}) # Manjaro
-        sudo pacman -Syyu
+        sudo -A pacman -Syyu
         ;;
     ${systems[10]}) # Gentoo
-        sudo emerge --sync && sudo emerge --update --deep --with-bdeps=y @world
+        sudo -A emerge --sync && sudo -A emerge --update --deep --with-bdeps=y @world
         ;;
 
     esac
---
+++ lsu.py
@@ -0,0 +1,67 @@
+# Developer: NR-SkaterBoy
+# Github: https://github.com/NR-SkaterBoy
+# E-mail: nr.rick.dev@gmail.com
+# Linux Systems source package update
+
+# Import modules
+import os, sys, stat, subprocess
+import webbrowser
+from pickle import NONE
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+# print(subprocess.call("pwd"))
+
+# Set the files right
+os.chmod("bash/myaskpass.sh", stat.S_IRWXU)
+os.chmod("bash/source_update.sh", stat.S_IRWXU)
+os.chmod("bash/system_update.sh", stat.S_IRWXU)
+
+# Import Sys-update script
+def runningSystemUpdate():
+    subprocess.call("bash/system_update.sh")
+    subprocess.call("bash/source_update.sh")
+
+# Webopen
+def openMyWebsite():
+    webbrowser.open_new(r"https://richardneuvald.tk")
+
+def openMyGithub():
+    webbrowser.open_new(r"https://github.com/NR-SkaterBoy")
+
+# Application
+
+# Colors
+background_color = "#181d31"
+btn_color = "#42485d"
+
+root = Tk()
+
+# Window, title, icon, background
+root.geometry("800x450+50+50")
+root.configure(background=background_color)
+root.title('Linux Updater')
+# root.iconbitmap("icons/lsu.ico") 
+# root.tk.call("wm", "iconphoto", root._w, tk.PhotoImage("icons/logo2png"))
+
+# Title
+Label(root, text='System\nUpdater', bg=background_color, fg="#ffffff", font=('arial', 40, 'bold')).place(x=60, y=25)
+
+# Btn of sysupdate
+Button(root, text='Update your system', bg='#F0F8FF', width=20, font=('arial', 12, 'normal'), command=runningSystemUpdate).place(x=70, y=200)
+
+# Btn of my website
+Button(root, text='Check my Website', bg='#F0F8FF', width=20, font=('arial', 12, 'normal'), command=openMyWebsite).place(x=70, y=260)
+
+# Btn of my github profile
+Button(root, text='Check my Github profile', bg='#F0F8FF', width=20, font=('arial', 12, 'normal'), command=openMyGithub).place(x=70, y=320)
+
+
+# Pictures
+lsu_pic= Canvas(root, height=470, width=449, bg=background_color, borderwidth=0, highlightthickness=0)
+picture_file = PhotoImage(file = 'pictures/lsu.png')
+lsu_pic.create_image(470, 0, anchor=NE, image=picture_file)
+lsu_pic.place(x=285, y=54)
+
+root.mainloop()

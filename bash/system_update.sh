e80977d5e3d1755ab884ba6283681d11e831f83e
NR-SkaterBoy <nr.rick.dev@gmail.com>
2022-03-20 20:00:06 +0100

    Finished sysupdates

--- bash/system_update.sh
+++ bash/system_update.sh
@@ -20,23 +20,21 @@ fi
 
 git pull
 shell=True
-# Linux Systems
-systems=("ubuntu" "kali" "fedora" "raspbian")
 
 # Linux version ID
 . /etc/os-release
 
 
-if [[ $ID == ${systems[0]} ]] || [[ $ID == ${systems[1]} ]] || [[ $ID == ${systems[2]} ]] || [[ $ID == ${systems[3]} ]] || [[ $ID == ${systems[4]} ]] || [[ $ID == ${systems[5]} ]] || [[ $ID == ${systems[6]} ]] || [[ $ID == ${systems[7]} ]] || [[ $ID == ${systems[8]} ]] || [[ $ID == ${systems[9]} ]]; then
+if [[ $ID == "ubuntu" ]] || [[ $ID == "kali" ]] || [[ $ID == "raspbian" ]] || [[ $ID == "fedora" ]]; then
     case "$ID" in
     "ubuntu" | "kali" | "raspbian") # Ubuntu-Kali-Raspbian
         zenity --password --title=authentication | sudo -S apt update && sudo -S apt upgrade -y && sudo -S apt autoremove -y
         ;;
-    ${systems[2]}) # Fedora
+    "fedora") # Fedora
         zenity --password --title=Authentication | sudo -S dnf upgrade -y
         ;;
      esac
-    zenity --notification --text "Succesful Update!" 
+
     # System restart
     if zenity --question --title="Restart" --text="Please restart the system!\n\nWould you like to restart the system?" --no-wrap; then
         zenity --password --title=Authentication | shutdown -r now

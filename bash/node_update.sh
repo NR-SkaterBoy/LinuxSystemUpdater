be705c73763196dbd0bf9c8df60dfcebc3620c85
NR-SkaterBoy <nr.rick.dev@gmail.com>
2022-04-18 11:22:44 +0200

    Node checker

--- bash/node_update.sh
+++ bash/node_update.sh
@@ -7,8 +7,16 @@
 # PASSWD=$(zenity --password --title=authentication)
 export SUDO_ASKPASS="bash/pass.sh"
 
-if zenity --question --title="Node Update" --text="Would you like to run node update?" --no-wrap; then
-    sudo -A npm cache clean -f
-    sudo -A npm install -g n
-    sudo -A n stable
+if which mode > /dev/null
+    then
+        if zenity --question --title="Node Update" --text="Would you like to run node update?" --no-wrap; then
+            sudo -A npm cache clean -f
+            sudo -A npm install -g n
+            sudo -A n stable
+        fi
+    else
+        if zenity --question --title="Node Installing..." --text="Would you like to install node package?" --no-wrap; then
+            sudo -A apt install node
+            sudo -A apt install npm
+        fi
 fi
